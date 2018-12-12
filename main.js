caa2b9851de6d5619a4f527450e7ab5532510ef9
Kinga Zaskórska <kingazas@outlook.com>
2018-12-12 23:32:19 +0100

    clock

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 
-function timer (){
-    var dzien= newDate();
-    var godzina= godzina.getHours();
-    var minuta= minuta.getMinutes();
-    var sekunda= sekunda.getSeconds();
+    function odliczanie()
+    	{
+    		var dzisiaj = new Date();
 
-    document.getElementById('timer').innerHTML= dzien;}
-    
+
+
+    		var godzina = dzisiaj.getHours();
+    		if (godzina<10) godzina = "0"+godzina;
+
+    		var minuta = dzisiaj.getMinutes();
+    		if (minuta<10) minuta = "0"+minuta;
+
+    		var sekunda = dzisiaj.getSeconds();
+    		if (sekunda<10) sekunda = "0"+sekunda;
+
+    		document.getElementById("zegar").innerHTML =
+    		 +godzina+":"+minuta+":"+sekunda;
+
+    		 setTimeout("odliczanie()",1000);
+    	}
